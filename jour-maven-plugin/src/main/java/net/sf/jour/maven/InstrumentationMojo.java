9df914bfc28ee7e6bb659a93050f05903bd7c956
Vlad Skarzhevskyy <skarzhevskyy@gmail.com>
2006-12-06 07:14:25 +0000

    integration tests
    git-svn-id: svn://svn.code.sf.net/p/jour/code@7 4bcd6a91-e123-0410-83ad-aaba895dfdbc

---
+++ jour-maven-plugin/src/main/java/net/sf/jour/maven/InstrumentationMojo.java
@@ -0,0 +1,35 @@
+package net.sf.jour.maven;
+
+import java.io.File;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.logging.Log;
+
+/**
+ * The jour:instrument will apply instrumentation during build.
+ * 
+ * @author vlads
+ *
+ * @goal instrument
+ *
+ * @description Instrument
+ */
+public class InstrumentationMojo extends AbstractMojo {
+
+	private Log log;
+
+	/**
+	 * Reads configuration options from the given file.
+	 * 
+	 * @parameter default-value="${basedir}/jour.xml"
+	 */
+	private File jourConfig;
+	
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		log = getLog();
+		
+		log.info(jourConfig.getAbsolutePath());
+	}
+}
