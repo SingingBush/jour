5657b9e504a2e3001b148be3d4c0705ef1d124d0
singingbush <singingbush@hotmail.com>
2022-07-06 08:46:30 +0100

    fixed problems in ReplaceMethodInstrumentor

--- jour-instrument/src/main/java/net/sf/jour/instrumentor/ReplaceMethodInstrumentor.java
+++ jour-instrument/src/main/java/net/sf/jour/instrumentor/ReplaceMethodInstrumentor.java
@@ -10,17 +10,32 @@ import net.sf.jour.util.StringUtil;
 public class ReplaceMethodInstrumentor  extends AbstractInstrumentor {
 
 	private String code;
-	
+
 	private String oldMethodSufix = "$orig";
-	
-	public void code(String code) {
+
+    public ReplaceMethodInstrumentor() {
+    }
+
+    /**
+     * Convenience for defining source code used for replacement which would otherwise be defined using setter
+     * @param code the source code that will be used to replace method body
+     * @since 2.1.1
+     */
+    public ReplaceMethodInstrumentor(final String code) {
+        this.code = code;
+    }
+
+    /**
+     * @param code the source code that will be used to replace method body
+     */
+    public void code(String code) {
 		this.code = code;
 	}
-	
+
 	public void oldMethodSufix(String oldMethodSufix) {
 		this.oldMethodSufix = oldMethodSufix;
 	}
-	
+
 	public boolean instrumentClass(CtClass clazz) throws InterceptorException {
 		return false;
 	}
@@ -31,17 +46,36 @@ public class ReplaceMethodInstrumentor  extends AbstractInstrumentor {
 
 	public boolean instrumentMethod(CtClass clazz, CtMethod method) throws InterceptorException {
 		try {
-			String mname = method.getName();
+            final String mname = method.getName();
 			//  rename old method to synthetic name, then duplicate the
 			//  method with original name for use as interceptor
-			String nname = mname + oldMethodSufix;
+			final String nname = mname + oldMethodSufix;
 			method.setName(nname);
 
-			CtMethod mnew = CtNewMethod.copy(method, mname, clazz, null);
+            final CtMethod mnew = CtNewMethod.copy(method, mname, clazz, null);
 
 			// replace the body of the interceptor method with generated
 			// code block and add it to class
-			mnew.setBody(StringUtil.replaceAll(code, "$origMethodName", nname));
+            if(code == null) {
+                log.warn("No replacement source code provided. %s method will be replaced by empty implementation");
+
+                final CtClass returnType = method.getReturnType();
+
+                if(method.getReturnType() == null) {
+                    mnew.setBody(StringUtil.replaceAll("", "$origMethodName", nname));
+                } else {
+                    if(returnType.isPrimitive()) {
+                        log.warn("Return type is primitive so cannot return null (default behaviour for no provided source).");
+                        throw new InterceptorException("Cannot replace method body " + method.getName() + " without replacement source being provided");
+                    }
+
+                    // todo: handle Optional<> when min Java version increased. Default to return Optional.empty();
+
+                    mnew.setBody(StringUtil.replaceAll("return null;", "$origMethodName", nname));
+                }
+            } else {
+                mnew.setBody(StringUtil.replaceAll(code, "$origMethodName", nname));
+            }
 			clazz.addMethod(mnew);
 		} catch (Exception e) {
 			throw new InterceptorException("Failed to replace method body " + method.getName(), e);
