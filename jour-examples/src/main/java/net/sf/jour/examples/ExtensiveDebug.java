986b60495a03a451834beb0da20ff3b8df459e33
singingbush <singingbush@hotmail.com>
2022-09-07 21:49:27 +0100

    added more examples to jour-examples module

--- jour-examples/src/main/java/net/sf/jour/examples/ExtensiveDebug.java
+++ jour-examples/src/main/java/net/sf/jour/examples/ExtensiveDebug.java
@@ -1,15 +1,41 @@
 package net.sf.jour.examples;
 
+/*
+* The generated class file will have code changes based on the configuration in
+* the process-classes.jour.xml file in the root of the jour-examples module.
+*/
 public class ExtensiveDebug {
 
-	String debugCalled;
-	
+	private boolean debugMethodBodyExists = false;
+
+    public ExtensiveDebug() {}
+
 	public ExtensiveDebug(String param) {
 		debug("ExtensiveDebug created", param);
 	}
 
-	public void debug(String message, String v) {
-		debugCalled = v;
+    // will have method body removed by MakeEmptyMethodInstrumentor
+    public void debug(String message, String v) {
+        debugMethodBodyExists = true;
 		System.out.println(message + " " + v);
 	}
+
+    public boolean debugMethodBodyRemoved() {
+        return !debugMethodBodyExists;
+    }
+
+    // will have body replaced by ReplaceMethodInstrumentor
+    public boolean replaceMe() {
+        return false;
+    }
+
+    // will have body replaced by ReplaceMethodInstrumentor
+    public String replaceThis() {
+        return "original text";
+    }
+
+    // will output execution time due to MethodExecutionTimeInstrumentor
+    public void timed() {
+        System.out.println("This should be timed");
+    }
 }
--- jour-examples/src/test/java/net/sf/jour/examples/ExtensiveDebugTest.java
+++ jour-examples/src/test/java/net/sf/jour/examples/ExtensiveDebugTest.java
@@ -2,14 +2,23 @@ package net.sf.jour.examples;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ExtensiveDebugTest {
 
     @Test
 	public void testDebugRemoved() {
 		final ExtensiveDebug e = new ExtensiveDebug("is removed?");
-		assertNull(e.debugCalled, "debug function called");
+
+		assertTrue(e.debugMethodBodyRemoved(), "debug function called but the functionality is removed");
 	}
 
+    @Test
+    public void testReplaceMethodInstrumentor() {
+        final ExtensiveDebug ed = new ExtensiveDebug();
+
+        assertTrue(ed.replaceMe(), "Should have been replaced with \"return true;\"");
+
+        assertEquals("it works!", ed.replaceThis(), "The result was originally \"original text\"");
+    }
 }
